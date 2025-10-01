b36acc44c7bed7003b0a0a99fc75f127fa3b0003
Christian S. <christianestebansr@gmail.com>
2025-10-01 18:18:35 -0500

    Created teleop file with Kiran's code in there

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/TeleOp.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode; // For linear OpModes
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp; // For TeleOp OpModes
+import com.qualcomm.robotcore.hardware.DcMotor; // For DC motors
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+@TeleOp(name="4Wheel Drive OpMode", group="LinearOpMode")
+public class main extends LinearOpMode {
+
+
+    // Define motor variables for 4 wheel drive
+    public DcMotor frontLeft;
+    public DcMotor frontRight;
+    public DcMotor backLeft;
+    public DcMotor backRight;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Init hardwareMaps for each motor
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        // Set directions for each motor
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+
+        // update telemetry to show INITIALIZED status
+        telemetry.addData("Status", "INITIALIZED");
+        telemetry.update();
+
+        // wait for user to press start button
+        waitForStart();
+
+        // start OpMode loop
+        while (opModeIsActive()) {
+            // get data from controller
+            double axial = -gamepad1.left_stick_y; // forward/backward driving
+            double lateral = gamepad1.left_stick_x; // strafing
+            double yaw = gamepad1.right_stick_x; // turning
+
+            // Calculate motor powers
+            double frontLeftPower = axial + lateral + yaw;
+            double frontRightPower = axial - lateral - yaw;
+            double backLeftPower = axial - lateral + yaw;
+            double backRightPower = axial + lateral - yaw;
+
+            // limit max motor power
+            frontLeftPower = Math.max(-1.0,Math.min(1.0, frontLeftPower));
+            frontRightPower = Math.max(-1.0,Math.min(1.0, frontRightPower));
+            backLeftPower = Math.max(-1.0,Math.min(1.0, backLeftPower));
+            backRightPower = Math.max(-1.0,Math.min(1.0, backRightPower));
+
+            // set motor power based on values
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
+
+            // Telemetry
+            telemetry.addData("Status", "RUNNING");
+            telemetry.addData("FrontLeft Motor Power", frontLeftPower);
+            telemetry.addData("FrontRight Motor Power", frontRightPower);
+            telemetry.addData("BackLeft Motor Power", backLeftPower);
+            telemetry.addData("BackRight Motor Power", backRightPower);
+            telemetry.update();
+        }
+    }
+}
